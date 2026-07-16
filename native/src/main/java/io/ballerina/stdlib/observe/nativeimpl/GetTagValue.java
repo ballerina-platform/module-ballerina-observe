b5080d1431ecc600ec1ffae58f90d1dbbecdde78
Thevakumar-Luheerathan <Luhee27@gmail.com>
2026-07-16 15:13:48 +0530

    Revert Java source changes to main

--- native/src/main/java/io/ballerina/stdlib/observe/nativeimpl/GetTagValue.java
+++ native/src/main/java/io/ballerina/stdlib/observe/nativeimpl/GetTagValue.java
@@ -28,6 +28,14 @@ public class GetTagValue {
         if (ObserveUtils.isObservabilityEnabled()) {
             ObserverContext observerContext = ObserveUtils.getObserverContextOfCurrentFrame(env);
             if (observerContext == null) {
+                try {
+                    String value = ObserveUtils.getCustomTag(tagKey.getValue());
+                    if (value != null) {
+                        return StringUtils.fromString(value);
+                    }
+                } catch (NoSuchMethodError e) {
+                    return null;
+                }
                 return null;
             }
             Tag tag = observerContext.getTag(tagKey.getValue());
